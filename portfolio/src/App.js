7bb0361619b9d392da93c78f00d7a2aaa57e78a8
Joshua Draper <josh.drapergame@gmail.com>
2022-09-01 22:16:08 -0600

    test commit/push

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           {/* I'm leaving this stuff in, I want to learn about the keyframes rotating the logo */}
+          {/* adding a comment so I have something to commit and push to github */}
       </header>
     </div>
   );
