6d228af7a4f6340fda6a04549079ce4f6bb83d0a
Joshua Draper <josh.drapergame@gmail.com>
2022-09-05 21:41:29 -0600

    I have a nav with which I can see things

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,12 +5,12 @@ import Navigation from './Navigation/Navigation';
 function App() {
   return (
     <div className="App">
+      <Navigation></Navigation>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           {/* I'm leaving this stuff in, I want to learn about the keyframes rotating the logo */}
           {/* adding a comment so I have something to commit and push to github */}
       </header>
-      <Navigation></Navigation>
     </div>
   );
 }
