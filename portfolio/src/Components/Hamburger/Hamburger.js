5ffa89de3eb82320ce9372da27c358b21ace2c0b
Joshua Draper <josh.drapergame@gmail.com>
2022-09-07 22:07:42 -0600

    test

--- portfolio/src/Components/Hamburger/Hamburger.js
+++ portfolio/src/Components/Hamburger/Hamburger.js
@@ -5,6 +5,7 @@ function Hamburger() {
   return (
     <div className="Hamburger">
       <img src={burger} className="Burger" alt="Hamburger" />
+      {/* commit comment, something seems to be broken on the local main */}
     </div>
   );
 }
