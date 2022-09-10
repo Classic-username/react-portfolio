916e3e4308e3b980d8023c6f7b59584195717db2
Joshua Draper <josh.drapergame@gmail.com>
2022-09-10 16:47:13 -0600

    created component scaffolding, for some reason there isn't anything rendering on the routes though. Need to see why

---
+++ portfolio/src/Components/Languages/Languages.js
@@ -0,0 +1,11 @@
+import './Languages.css';
+
+function Languages() {
+  return (
+    <div className="Languages">
+        There is nothing here... YET. SOON THERE SHALL BE "Languages" THINGS.
+    </div>
+  );
+}
+
+export default Languages;
---
+++ portfolio/src/Components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="Projects">
+        There is nothing here... YET. SOON THERE SHALL BE "Projects" THINGS.
+    </div>
+  );
+}
+
+export default Projects;
